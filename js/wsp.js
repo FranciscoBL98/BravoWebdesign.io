11b17db04959fd7d91336b58287266b76343ffaf
FranciscoBL98 <fbignacio.98@gmail.com>
2025-01-20 17:47:50 -0300

    Update wsp.js

--- js/wsp.js
+++ js/wsp.js
@@ -18,7 +18,7 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/+56991466933?text='+relmsg, '_blank'); 
+   window.open('https://wa.me/+56991466833?text='+relmsg, '_blank'); 
   
   });
 
